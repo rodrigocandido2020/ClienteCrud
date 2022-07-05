ca9ac87e0883f45c13f3b1a049e6969f2a9e6863
rodrigocanadido2020 <primoscandido93@gmail.com>
2022-07-05 15:19:40 -0300

    Controller API

---
+++ Api.Crud.Usuario/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using Crud.Dominio;
+using Crud.Infra;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Crud.Usuario.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        [Route("ObterTodosOsUsuarios")]
+        public IActionResult ObterTodosOsUsuarios()
+        {
+            var todosUsuarios = _usuarioRepositorio.ObterTodos();
+            return Ok(todosUsuarios);
+        }
+
+        [HttpGet]
+        [Route("ObterPorId")]
+        public IActionResult ObterPorIdUsuario(int id)
+        {
+            if(id == 0)
+            {
+                return new OkObjectResult(new { message = "Usuário não encontrado" });
+            }
+            var IdUsuario = _usuarioRepositorio.ObterPorId(id);
+            return Ok(IdUsuario);
+        }
+    }
+}
---
+++ Api.Crud.Usuario/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Crud.Dominio;
+using Crud.Infra;
+using Microsoft.OpenApi.Models;
+
+namespace Api.Crud.Usuario
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            MapeamentoDeTabela.Mapear();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IUsuarioRepositorio, UsuarioRepositorioComLinqDb>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUD.WebApp", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aplicação CRUD Swagger UI"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
